a66ce388d00626b8c9a770c4e7702c454189424d
pradiptadh <haryadipradipta@gmail.com>
2022-03-09 17:18:36 +0700

    simple api with spring boot

---
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -0,0 +1,12 @@
+package com.springboot.blog.payload;
+
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
---
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -0,0 +1,8 @@
+package com.springboot.blog.service;
+
+import com.springboot.blog.payload.PostDto;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto) ;
+
+}
